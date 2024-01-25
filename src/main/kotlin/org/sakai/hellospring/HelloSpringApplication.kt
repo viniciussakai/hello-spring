301796bd7dd13f5ae7d5461f8ea7ff3ad98e7a22
Vinicius Sakai <vinicius.sakai@tuta.io>
2024-01-25 16:40:53 -0300

    inital commit

---
+++ src/main/kotlin/org/sakai/hellospring/HelloSpringApplication.kt
@@ -0,0 +1,11 @@
+package org.sakai.hellospring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelloSpringApplication>(*args)
+}
---
+++ src/test/kotlin/org/sakai/hellospring/HelloSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sakai.hellospring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
